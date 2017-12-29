df37122e80071202a3009d1f88adc250bf9b0071
Eric Chan <eric.chan@intellihr.com.au>
2017-12-29 15:47:18 +1000

    export execPromisify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Resp from './response'
 import { validate as jsv } from './jsonSchema'
-import { awsPromisify as awsp } from './promisify'
+import { awsPromisify as awsp, execPromisify as execp } from './promisify'
 
 export const Response = Resp
 export const jsValidate = jsv
 export const awsPromisify = awsp
+export const execPromisify = execp
