92bbac8c315eaec554a63eef4ec055783e222c28
Soloman Weng <soloman1124@gmail.com>
2018-01-11 11:53:10 +1000

    Add build file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { validate as jsv } from './jsonSchema'
 import { awsPromisify as awsp, execPromisify as execp } from './promisify'
 import { kinesis as kin } from './kinesis'
 import { dynamoDB as dyn } from './dynamoDB'
+import { s3 as _s3 } from './s3'
 
 export const Response = Resp
 export const jsValidate = jsv
@@ -10,3 +11,4 @@ export const awsPromisify = awsp
 export const execPromisify = execp
 export const kinesis = kin
 export const dynamoDB = dyn
+export const s3 = _s3
