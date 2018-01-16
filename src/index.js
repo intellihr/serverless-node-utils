0238b1147d07575ac8f184bf325bbb4438834023
Blake Taylor <sadondesu+ihr@gmail.com>
2018-01-16 15:14:46 +1000

    Add support for SQS

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { awsPromisify as awsp, execPromisify as execp } from './promisify'
 import { kinesis as kin } from './kinesis'
 import { dynamoDB as dyn } from './dynamoDB'
 import { s3 as _s3 } from './s3'
+import { sqs as _sqs } from './sqs'
 
 export const Response = Resp
 export const jsValidate = jsv
@@ -12,3 +13,4 @@ export const execPromisify = execp
 export const kinesis = kin
 export const dynamoDB = dyn
 export const s3 = _s3
+export const sqs = _sqs
---
+++ src/sqs.js
@@ -0,0 +1,27 @@
+import aws from 'aws-sdk'
+import { awsPromisify } from './promisify'
+
+function sqsHelper (aws, process, promisify) {
+  const {
+    IS_OFFLINE,
+    SQS_ENDPOINT
+  } = process.env
+
+  let options = {}
+
+  if (IS_OFFLINE) {
+    options = {
+      region: 'localhost',
+      endpoint: SQS_ENDPOINT,
+      credentials: new aws.Credentials('accessKey', 'secretKey')
+    }
+  }
+
+  const sqs = new aws.SQS(options)
+
+  return {
+    sendMessage: promisify(::sqs.sendMessage)
+  }
+}
+
+export const sqs = sqsHelper(aws, process, awsPromisify)
